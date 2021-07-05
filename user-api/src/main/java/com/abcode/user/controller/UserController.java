f0bcbcdaa853338462a5c8e174932c25cbd747d3
Arthur Barbosa <arthurbruno03@gmail.com>
2021-07-04 20:50:33 -0400

    Created class controller user and change config open view jpa

--- user-api/src/main/java/com/abcode/user/controller/UserController.java
+++ user-api/src/main/java/com/abcode/user/controller/UserController.java
@@ -1,77 +1,47 @@
 package com.abcode.user.controller;
 
 import com.abcode.user.dto.UserDTO;
+import com.abcode.user.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    public static List<UserDTO> users = new ArrayList<UserDTO>();
+    private final UserService userService;
 
-    @PostConstruct
-    public void initiateList() {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setName("Eduardo");
-        userDTO.setCpf("123");
-        userDTO.setAddress("Rua a");
-        userDTO.setEmail("eduardo@email.com");
-        userDTO.setPhone("1234-3454");
-        userDTO.setDateRegister(new Date());
-        UserDTO userDTO2 = new UserDTO();
-        userDTO2.setName("Luiz");
-        userDTO2.setCpf("456");
-        userDTO2.setAddress("Rua b");
-        userDTO2.setEmail("luiz@email.com");
-        userDTO2.setPhone("1234-3454");
-        userDTO2.setDateRegister(new Date());
-        UserDTO userDTO3 = new UserDTO();
-        userDTO3.setName("Bruna");
-        userDTO3.setCpf("678");
-        userDTO3.setAddress("Rua c");
-        userDTO3.setEmail("bruna@email.com");
-        userDTO3.setPhone("1234-3454");
-        userDTO3.setDateRegister(new Date());
-        users.add(userDTO);
-        users.add(userDTO2);
-        users.add(userDTO3);
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
-
 
     @GetMapping("/users")
     public List<UserDTO> getUsers() {
-        return users;
+        return userService.getAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public UserDTO findById(@PathVariable Long id) {
+        return userService.findById(id);
     }
 
     @GetMapping("/users/{cpf}")
     public UserDTO getUserFilter(@PathVariable String cpf) {
-        for (UserDTO userFilter : users) {
-            if (userFilter.getCpf().equals(cpf)) {
-                return userFilter;
-            }
-        }
-        return null;
+        return userService.findByCpf(cpf);
     }
 
-    @PostMapping("/users/new-user")
-    public UserDTO insert(@RequestBody UserDTO userDTO) {
-        userDTO.setDateRegister(new Date());
-        users.add(userDTO);
-        return userDTO;
+    @PostMapping("/users")
+    public UserDTO save(@RequestBody UserDTO userDTO) {
+        return userService.save(userDTO);
     }
 
-    @DeleteMapping("/users/{cpf}")
-    public boolean remove(@PathVariable String cpf) {
-        for (UserDTO userFilter : users) {
-            if (userFilter.getCpf().equals(cpf)) {
-                users.remove(userFilter);
-                return true;
-            }
-        }
-        return false;
+    @DeleteMapping("/users/{id}")
+    public UserDTO remove(@PathVariable Long id) {
+        return userService.delete(id);
+    }
+
+    @GetMapping("/users/search")
+    public List<UserDTO> queryByName(@RequestParam(name = "name", required = true) String name) {
+        return userService.queryByName(name);
     }
 }
