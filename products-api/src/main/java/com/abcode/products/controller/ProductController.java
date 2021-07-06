68e5026861d34d801286c0f3ac3cdc849d74b6f2
Arthur Barbosa <arthurbruno03@gmail.com>
2021-07-05 20:29:47 -0400

    created  product controller

---
+++ products-api/src/main/java/com/abcode/products/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.abcode.products.controller;
+
+import com.abcode.products.dto.ProductDTO;
+import com.abcode.products.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/product")
+    public List<ProductDTO> getProducts() {
+        List<ProductDTO> produtos = productService.getAll();
+        return produtos;
+    }
+
+    @GetMapping("/product/category/{categoryId}")
+    public List<ProductDTO> getProductByCategory(@PathVariable Long categoryId) {
+        return productService.getProductByCategoryId(categoryId);
+    }
+
+    @GetMapping("/product/{productIdentifier}")
+    ProductDTO findById(@PathVariable String productIdentifier) {
+        return productService.findByProductIdentifier(productIdentifier);
+    }
+
+    @PostMapping("/product")
+    ProductDTO newProduct(@Valid @RequestBody ProductDTO productDTO) {
+        return productService.save(productDTO);
+    }
+
+    @DeleteMapping("/product/{id}")
+    ProductDTO delete(@PathVariable Long id) throws Exception {
+        return productService.delete(id);
+    }
+}
